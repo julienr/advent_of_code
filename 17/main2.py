9ad23d4c683c91c92a83e37beb840faa9aac0dc1
Julien Rebetez <julien@fhtagn.net>
2020-12-19 21:24:09 +0100

    Day 17

---
+++ 17/main2.py
@@ -0,0 +1,81 @@
+import numpy as np
+with open('input') as f:
+    lines = [l for l in f.read().split('\n') if l.strip() != '']
+    state = np.array([[[list(l) for l in lines]]])
+print(state)
+
+
+def print_state(state):
+    for w in range(state.shape[0]):
+        for z in range(state.shape[1]):
+            s = state[w, z]
+            print('w=%d, z=%d' % (w, z))
+            for i in range(s.shape[0]):
+                print(''.join(list(s[i])))
+            print()
+
+
+def get_neighbors(state, w, z, i, j):
+    neigh = []
+    for dw in [-1, 0, 1]:
+        for dz in [-1, 0, 1]:
+            for di in [-1, 0, 1]:
+                for dj in [-1, 0, 1]:
+                    if dw == dz == di == dj == 0:
+                        continue
+                    neigh.append(get_state(state, w + dw, z + dz, i + di, j + dj))
+    return np.array(neigh)
+
+
+def get_state(state, w, z, i, j):
+    if w < 0 or w >= state.shape[0]:
+        return '.'
+    if z < 0 or z >= state.shape[1]:
+        return '.'
+    if i < 0 or i >= state.shape[2]:
+        return '.'
+    if j < 0 or j >= state.shape[3]:
+        return '.'
+    return state[w, z, i, j]
+
+
+def step(state):
+    new_state = np.zeros((
+        state.shape[0] + 2,
+        state.shape[1] + 2,
+        state.shape[2] + 2,
+        state.shape[3] + 2,
+    ), dtype=state.dtype)
+
+    for nw in range(new_state.shape[0]):
+        for nz in range(new_state.shape[1]):
+            for ni in range(new_state.shape[2]):
+                for nj in range(new_state.shape[3]):
+                    w = nw - 1
+                    z = nz - 1
+                    i = ni - 1
+                    j = nj - 1
+                    curr = get_state(state, w, z, i, j)
+                    neighbors = get_neighbors(state, w, z, i, j)
+                    if curr == '#':
+                        num_neigh_active = np.count_nonzero(neighbors == '#')
+                        if num_neigh_active == 2 or num_neigh_active == 3:
+                            new_state[nw, nz, ni, nj] = '#'
+                        else:
+                            new_state[nw, nz, ni, nj] = '.'
+                    elif curr == '.':
+                        num_neigh_active = np.count_nonzero(neighbors == '#')
+                        if num_neigh_active == 3:
+                            new_state[nw, nz, ni, nj] = '#'
+                        else:
+                            new_state[nw, nz, ni, nj] = '.'
+
+    return new_state
+
+
+for cycle in range(6):
+    print('After %d cycles' % cycle)
+    print_state(state)
+    state = step(state)
+
+    print('%d active' % np.count_nonzero(state == '#'))
