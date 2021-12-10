15f906c09ab6af3fa966e45cf001847c79335919
Julien Rebetez <julien@fhtagn.net>
2021-12-10 08:30:20 +0100

    2021 day 4

---
+++ 2021/4/main.py
@@ -0,0 +1,55 @@
+import numpy as np
+import sys
+
+boards = []
+draw_order = []
+
+with open('input') as f:
+    draw_order = [int(c) for c in f.readline().split(',')]
+
+    for line in f:
+        if line.strip() == '':
+            # Start new board
+            board = np.zeros([5, 5], dtype=np.int32)
+            for i in range(5):
+                line = f.readline()
+                row = np.array([int(c) for c in line.rsplit(' ') if c != ''])
+                board[i, :] = row
+            boards.append(board)
+
+marks = [np.zeros((5, 5), dtype=np.bool) for board in boards]
+
+def check_number(number, board, mark):
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if board[i, j] == number:
+                mark[i, j] = True
+
+    # Check win status
+    for i in range(board.shape[0]):
+        if np.all(mark[i]):
+            return True
+    for j in range(board.shape[1]):
+        if np.all(mark[:, j]):
+            return True
+    return False
+
+
+def score(number, board, mark):
+    score = 0
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if not mark[i, j]:
+                score += board[i, j]
+    score *= number
+    return score
+
+
+for number in draw_order:
+    for i, board in enumerate(boards):
+        if check_number(number, board, marks[i]):
+            print(score(number, board, marks[i]))
+            sys.exit(0)
+
+sys.exit(-1)
+
